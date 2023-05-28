5e89a18595425ff020dc3596ac4769c7a2a199da
GabsonRogeer <gabsonwona@gmail.com>
2023-05-28 04:45:22 -0300

    completing form, add cards

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -5,10 +5,14 @@ const CampoTexto = (props) => {
     const aoDigitado = (evento) => {
         props.aoAlterado(evento.target.value)
     }
+
     return (
         <div className='campo-texto'>
-            <label>{props.label}</label>
-            <input value={props.value}
+            <label>
+                {props.label}
+            </label>
+            <input  
+            value={props.valor}
             onChange={aoDigitado}
             required={props.obrigatorio} 
             placeholder={props.placeholder}/>
